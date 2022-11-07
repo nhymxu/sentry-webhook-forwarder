03104b54f034cac505ec26e098290c3ca1c5d3b9
Dung Nguyen <contact@dungnt.net>
2022-11-07 22:24:27 +0700

    feat: support telegram (#2)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# slack-sentry-bot
+# sentry-webhook-forwarder
 Receive sentry webhook message, re-format and send to slack.
 
 
@@ -6,6 +6,13 @@ Receive sentry webhook message, re-format and send to slack.
 Go to Vercel dashboard, and setup two environment variable:
 - `API_KEY`: secret key to protect your app
 - `SLACK_WEBHOOK`: your slack webhook to post message
+- `TELEGRAM_WEBHOOK`: your telegram bot webhook to post message
+Format normally like that:
+```
+TELEGRAM_CHAT_ID = 'channel/group chat id here'
+TELEGRAM_BOT_TOKEN = 'bot token here'
+TELEGRAM_WEBHOOK = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&parse_mode=HTML&disable_web_page_preview=true'
+```
 
 ## Deploy
 - Install Vercel CLI not you don't have
--- api.py
+++ api.py
@@ -12,14 +12,9 @@ logger.setLevel(logging.INFO)
 
 API_KEY = os.getenv('API_KEY')
 SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
+TELEGRAM_WEBHOOK = os.getenv('TELEGRAM_WEBHOOK')
 
 app = FastAPI()
-
-SLACK_MSG_COLORS = {
-    'warning': '#f2c744',
-    'error': '#e70707',
-    'info': '#d0d0d0'
-}
 
 
 async def parse_event_tags(tag_list):
@@ -32,7 +27,7 @@ async def parse_event_tags(tag_list):
     return {r[0]: r[1] for r in tag_list}
 
 
-async def build_slack_message_block(msg):
+async def _build_slack_message_block(msg):
     """
     Build Slack block message
 
@@ -41,7 +36,7 @@ async def build_slack_message_block(msg):
     :param msg:
     :return:
     """
-    project_slug = msg['project_slug']
+    project_slug = msg.get('project_slug')
 
     # project slug format: dev-project-name, stg-project-name, prod-project-name
     run_env = project_slug.split('-')[0]
@@ -49,7 +44,7 @@ async def build_slack_message_block(msg):
     tags = await parse_event_tags(msg.get('event', {}).get('tags', {}))
 
     dt = datetime.fromtimestamp(
-        msg['event']['timestamp'],
+        msg.get('event', {}).get('timestamp', datetime.now().timestamp()),
         tz=ZoneInfo('UTC')
     )
     local_dt = dt.astimezone(tz=ZoneInfo('Asia/Ho_Chi_Minh'))
@@ -67,7 +62,7 @@ async def build_slack_message_block(msg):
             "fields": [
                 {
                     "type": "mrkdwn",
-                    "text": f"*severity:*\n{msg['level'].upper()}"
+                    "text": f"*severity:*\n{msg.get('level', '').upper()}"
                 },
                 {
                     "type": "mrkdwn",
@@ -113,13 +108,60 @@ async def build_slack_message_block(msg):
     ]
 
 
+async def _build_telegram_message_block(msg: dict) -> str:
+    """
+    Build telegram block message
+
+    :param msg:
+    :return:
+    """
+    project_slug = msg.get('project_slug')
+
+    # project slug format: dev-project-name, stg-project-name, prod-project-name
+    run_env = project_slug.split('-')[0]
+
+    tags = await parse_event_tags(msg.get('event', {}).get('tags', {}))
+
+    dt = datetime.fromtimestamp(
+        msg.get('event', {}).get('timestamp', datetime.now().timestamp()),
+        tz=ZoneInfo('UTC')
+    )
+    local_dt = dt.astimezone(tz=ZoneInfo('Asia/Ho_Chi_Minh'))
+
+    output_message = f"Sentry / {project_slug} - {local_dt}\n"
+
+    output_message += f"*message*:\n{msg.get('message')}"
+
+    output_message += "\n\n#notification"
+
+    return output_message
+
+
 @app.get('/')
 async def catch_all_other():
+    """
+    Other catch all function for vercel
+
+    :return:
+    """
     return {'msg': 'Nothing here'}
 
 
-@app.post('/webhook/sentry/{api_key}')
-async def webhook_sentry(api_key: str, request: Request):
+@app.post('/webhook/slack/{api_key}')
+async def webhook_slack(api_key: str, request: Request):
+    """
+    Endpoint receive sentry message and forward to Slack
+
+    :param api_key:
+    :param request:
+    :return:
+    """
+    SLACK_MSG_COLORS = {
+        'warning': '#f2c744',
+        'error': '#e70707',
+        'info': '#d0d0d0'
+    }
+
     if api_key != API_KEY:
         raise HTTPException(status_code=404, detail="Project not found")
 
@@ -133,7 +175,7 @@ async def webhook_sentry(api_key: str, request: Request):
         'attachments': [
             {
                 "color": f"{color}",
-                "blocks": await build_slack_message_block(issue)
+                "blocks": await _build_slack_message_block(issue)
             }
         ]
     }
@@ -147,5 +189,38 @@ async def webhook_sentry(api_key: str, request: Request):
         logger.error(e)
         raise e
     # End slack
+
+    # return slack_msg
+    return {'msg': 'ok'}
+
+
+@app.post('/webhook/telegram/{api_key}')
+async def webhook_telegram(api_key: str, request: Request):
+    """
+    Endpoint receive sentry message and forward to Telegram
+
+    :param api_key:
+    :param request:
+    :return:
+    """
+    if api_key != API_KEY:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    msg = await request.body()
+    issue = json.loads(msg)
+
+    message_builder = await _build_telegram_message_block(issue)
+
+    try:
+        url = f'{TELEGRAM_WEBHOOK}&text={message_builder}'
+        r = requests.get(url)
+        r.raise_for_status()
+
+        response = r.json()
+        if not response['ok']:
+            raise ValueError("Failed to send message to Telegram")
+    except Exception as e:
+        logger.error(e)
+        raise e
 
     return {'msg': 'ok'}
